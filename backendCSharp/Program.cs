c9df90b395df59d28f2f0237b715a4f089fe9c68
xd4rk1ng <rui.santos.maia@gmail.com>
2025-10-25 23:26:53 +0100

    C# transition commit

---
+++ backendCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace backendCSharp;
+
+using MongoDB.Driver;
+using MongoApi;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string connectionString = "mongodb://root:atec123@127.0.0.1:27017";
+        string databaseName = "library";
+        string collectionName = "students";
+
+        var client = new MongoClient(connectionString);
+        var db = client.GetDatabase(databaseName);
+        var collection = db.GetCollection<StudentModel>(collectionName);
+
+        var student = new StudentModel { FirstName = "Joao", LastName = "Sharp" };
+
+        await collection.InsertOneAsync(student);
+
+        var cursor = await collection.FindAsync(_ => true);
+        var results = cursor.ToList();
+
+        foreach(var result in results)
+        {
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+}
---
+++ backendCSharp/StudentModel.cs
@@ -0,0 +1,30 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace MongoApi;
+
+public class StudentModel
+{
+    /* These 2 lines and the property Id: */
+    //
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    /*  - [BsonId] tells MongoDB that this property represents the "_id" field in the document.*/
+    /*  - [BsonRepresentation(BsonType.ObjectId)] allows you to use a string in C#, while MongoDB stores it as an ObjectId.*/
+    //--------------------------------
+    /*  So:
+        - In C#, the property is called Id
+        - In MongoDB, it maps to the _id field*/
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public override string ToString()
+    {
+        
+        return $"ObjectId: {Id} -- {FirstName} {LastName}";
+    }
+
+}
